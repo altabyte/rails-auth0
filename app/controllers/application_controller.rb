7de52ec47ca3b7ac1b9c765dc9f74322fb95bb73
Rob Graham <altabyte@gmail.com>
2017-04-03 14:51:50 +0100

    Timestamp session after each action.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  after_action  :session_timestamp!
+
   helper_method :auth0_jwt
+
+  #---------------------------------------------------------------------------
+  private
+
+  # Capture timestamp after each action, so can later cleanup stale sessions.
+  #
+  def session_timestamp!
+    session['updated_at'] = Time.now.utc.to_i
+  end
 
   def auth0_jwt
     session[:userinfo]&.fetch('credentials')&.fetch('id_token')

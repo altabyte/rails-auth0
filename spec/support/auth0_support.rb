d9a4be17e5f7f8f4d4869bf6a5070173a79241d6
Rob Graham <altabyte@gmail.com>
2017-04-26 10:22:02 +0100

    Set RuboCop TargetRubyVersion to 2.4.

--- spec/support/auth0_support.rb
+++ spec/support/auth0_support.rb
@@ -27,53 +27,52 @@ module Auth0RequestSpecHelper
     # @see https://jwt.io/
     id_token ||= 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZW1haWwiOiJqb2huLmRvZUBleGFtcGxlLmNvbSIsImV4cCI6MTYwOTQ1OTE0MH0.gnCidNCNEGy4zPWqmdDIaMk3QHI10a2ckAb2txaxgWg'
 
-    json = <<EOQ
-{
-  "provider": "auth0",
-  "uid": "auth0|#{uid}",
-  "info": {
-    "name": "#{email}",
-    "email": "#{email}",
-    "nickname": "#{nickname}",
-    "first_name": null,
-    "last_name": null,
-    "location": null,
-    "image": null
-  },
-  "credentials": {
-    "token": "#{credentials_token}",
-    "expires": true,
-    "id_token": "#{id_token}",
-    "token_type": "Bearer"
-  },
-  "extra": {
-    "raw_info": {
-      "email_verified": #{email_verified},
-      "email": "#{email}",
-      "clientID": "#{Rails.application.secrets.auth0_client_id}",
-      "updated_at": "#{(Time.now.utc - 2.minutes).iso8601(3)}",
-      "name": "#{email}",
-      "picture": null,
-      "user_id": "auth0|#{uid}",
-      "nickname": "#{nickname}",
-      "identities": [
-        {
-          "user_id": "#{uid}",
-          "provider": "auth0",
-          "connection": "Username-Password-Authentication",
-          "isSocial": false
-        }
-      ],
-      "created_at": "#{(Time.now.utc - 2.days).iso8601(3)}",
-      "sub": "auth0|#{uid}"
-    }
-  }
-}
-EOQ
+    json = <<-EOQ
+             {
+               "provider": "auth0",
+               "uid": "auth0|#{uid}",
+               "info": {
+                 "name": "#{email}",
+                 "email": "#{email}",
+                 "nickname": "#{nickname}",
+                 "first_name": null,
+                 "last_name": null,
+                 "location": null,
+                 "image": null
+               },
+               "credentials": {
+                 "token": "#{credentials_token}",
+                 "expires": true,
+                 "id_token": "#{id_token}",
+                 "token_type": "Bearer"
+               },
+               "extra": {
+                 "raw_info": {
+                   "email_verified": #{email_verified},
+                   "email": "#{email}",
+                   "clientID": "#{Rails.application.secrets.auth0_client_id}",
+                   "updated_at": "#{(Time.now.utc - 2.minutes).iso8601(3)}",
+                   "name": "#{email}",
+                   "picture": null,
+                   "user_id": "auth0|#{uid}",
+                   "nickname": "#{nickname}",
+                   "identities": [
+                     {
+                       "user_id": "#{uid}",
+                       "provider": "auth0",
+                       "connection": "Username-Password-Authentication",
+                       "isSocial": false
+                     }
+                   ],
+                   "created_at": "#{(Time.now.utc - 2.days).iso8601(3)}",
+                   "sub": "auth0|#{uid}"
+                 }
+               }
+             }
+             EOQ
 
     hash = JSON.parse(json).with_indifferent_access
     jwt = hash[:credentials][:id_token]
-    # bearer = JSON.parse(Base64.decode64(jwt.split('.').first)).with_indifferent_access
     claims = JSON.parse(Base64.decode64(jwt.split('.').second)).with_indifferent_access
     puts claims.to_yaml
     puts "JWT expires:  #{Time.at(claims[:exp])}"

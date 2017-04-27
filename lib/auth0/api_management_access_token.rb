b2366d08848115cbd2a9389b6044050ed5b5069f
Rob Graham <altabyte@gmail.com>
2017-04-27 22:38:48 +0100

    Auth0 management API lib module.

---
+++ lib/auth0/api_management_access_token.rb
@@ -0,0 +1,119 @@
+require 'auth0'
+
+module Auth0
+  module API
+    # Get an API access token for the Auth0 API.
+    module ManagementAccessToken
+
+      # Get a JWT access token for the management API.
+      #
+      # @return [String] a valid JWT access token.
+      #
+      # @TODO Cache the access token in Redis and only request a new token if the cached one has expired.
+      #
+      def self.get
+        request_management_access_token!
+      end
+
+      def self.decode_token(token:)
+        JWT.decode token, nil, false
+      end
+
+      # Determine if the given JWT auth token has expired.
+      #
+      # @param [String] token the JWT token string to be evaluated.
+      #
+      # @return [Boolean] true if the token has expired.
+      #
+      def self.jwt_expired?(token:)
+        decoded_token = decode_token(token: token)
+        Time.now.to_i > decoded_token.reduce({}, :merge).fetch('exp', 0).to_i
+      end
+
+      # Get an array of the scopes available to this token.
+      #
+      # @param [String] token the JWT access token.
+      #
+      # @return [Array[String]] true if the token has expired.
+      #
+      def self.jwt_scopes(token:)
+        decoded_token = decode_token(token: token)
+        decoded_token.reduce({}, :merge).fetch('scope', '').split(' ')
+      end
+
+      #---------------------------------------------------------------------------
+      # private methods
+
+      def self.auth0_domain
+        ENV['AUTH0_DOMAIN']
+      end
+
+      def self.auth0_client_id
+        ENV['AUTH0_CLIENT_ID']
+      end
+
+      def self.auth0_secret
+        ENV['AUTH0_CLIENT_SECRET']
+      end
+
+      def self.auth0_uri(path:)
+        URI("https://#{auth0_domain}#{path}")
+      end
+
+      def self.management_api_credentials
+        {
+          grant_type:     'client_credentials',
+          client_id:      auth0_client_id,
+          client_secret:  auth0_secret,
+          audience:       "https://#{auth0_domain}/api/v2/"
+        }
+      end
+
+      def self.net_http(uri:)
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        http
+      end
+
+      #
+      # @raise [Auth0ManagementAPI::Error] if fails to acquire token.
+      def self.request_management_access_token!
+        uri = auth0_uri path: '/oauth/token'
+        request = Net::HTTP::Post.new(uri)
+        request['content-type'] = 'application/json'
+        request.body = management_api_credentials.to_json
+
+        response = net_http(uri: uri).request(request)
+        extract_response_access_token!(response)
+      end
+
+      def self.extract_response_access_token!(response)
+        body = JSON.parse response.read_body
+        validate_access_token_response!(response: response, body: body)
+        body['access_token']
+      end
+
+      def self.validate_access_token_response!(response:, body:)
+        raise Auth0::Exception, body['error_description'] if body.key? 'error_description'
+        raise Auth0::Exception, 'Access token in not a JWT Bearer' unless body['token_type'] == 'Bearer'
+        raise Auth0::Exception, 'Access token missing' unless body.key? 'access_token'
+        raise Auth0::Exception, "#{response.code}: Auth0 management token request failed" if response.code.to_i >= 300
+      end
+
+      #---------------------------------------------------------------------------
+      # Make the following methods private
+      #
+      private_class_method :auth0_domain,
+                           :auth0_client_id,
+                           :auth0_secret,
+                           :auth0_uri,
+                           :management_api_credentials,
+                           :net_http,
+                           :request_management_access_token!,
+                           :extract_response_access_token!,
+                           :validate_access_token_response!
+
+    end
+  end
+end
---
+++ spec/lib/auth0/api_management_access_token_spec.rb
@@ -0,0 +1,150 @@
+require 'rails_helper'
+require 'auth0'
+require 'auth0/api_management_access_token'
+
+describe 'Auth0::API::ManagementAccessToken' do
+
+  context 'Invalid credentials' do
+    it 'Fails when AUTH0_DOMAIN is invalid' do
+      ClimateControl.modify AUTH0_DOMAIN: 'INVALID' do
+        # Probably not necessary to test this.
+        # expect { Auth0::API::ManagementAccessToken.get }.to raise_error(SocketError)
+      end
+    end
+
+    it 'Fails when AUTH0_CLIENT_ID is invalid' do
+      ClimateControl.modify AUTH0_CLIENT_ID: 'INVALID' do
+        expect { Auth0::API::ManagementAccessToken.get }.to raise_error(Auth0::Exception)
+      end
+    end
+
+    it 'Fails when AUTH0_CLIENT_SECRET is invalid' do
+      ClimateControl.modify AUTH0_CLIENT_SECRET: 'INVALID' do
+        expect { Auth0::API::ManagementAccessToken.get }.to raise_error(Auth0::Exception)
+      end
+    end
+  end
+
+  describe 'private methods' do
+    let(:private_methods) do
+      %i[
+        auth0_domain
+        auth0_client_id
+        auth0_secret
+        auth0_uri
+        management_api_credentials
+        net_http
+        request_management_access_token!
+        extract_response_access_token!
+        validate_access_token_response!
+      ]
+    end
+
+    it 'does not respond to private methods' do
+      private_methods.each do |private_method|
+        expect(Auth0::API::ManagementAccessToken).not_to respond_to private_method
+      end
+    end
+  end
+
+  describe '#get' do
+    it { expect(Auth0::API::ManagementAccessToken).to respond_to :get }
+    it { expect { Auth0::API::ManagementAccessToken.get }.not_to raise_error }
+
+    describe 'token' do
+      before(:all) do
+        @token = Auth0::API::ManagementAccessToken.get
+        puts @token
+      end
+      let(:token) { @token }
+
+      it { expect(token).to be_a String }
+      it { expect(token.split('.').count).to eq 3 }
+      it { puts Auth0::API::ManagementAccessToken.decode_token(token: token) }
+      it { puts "Expires: #{Time.at Auth0::API::ManagementAccessToken.decode_token(token: token).first.fetch('exp', 0)}" }
+      it { expect(Auth0::API::ManagementAccessToken.jwt_expired?(token: token)).to be false }
+      it { expect(Auth0::API::ManagementAccessToken.jwt_scopes(token: token)).to be_a Array }
+      it { puts Auth0::API::ManagementAccessToken.jwt_scopes(token: token).join("\n") }
+    end
+  end
+
+  describe 'API calls using access token' do
+    before(:all) do
+      @token = Auth0::API::ManagementAccessToken.get
+      @auth0 = Auth0Client.new(client_id: ENV.fetch('AUTH0_CLIENT_ID', ''),
+                               token: @token,
+                               domain: ENV.fetch('AUTH0_DOMAIN', ''),
+                               timeout: 30.seconds,
+                               api_version: 2)
+
+    end
+    let(:auth0_api) { @auth0 }
+
+    describe '#get_users' do
+      describe 'getting a page of users' do
+        let(:page)     { 0 }
+        let(:per_page) { 50 }
+
+        subject(:users) { auth0_api.get_users per_page: per_page, page: page }
+
+        it { puts users }
+        it { is_expected.to be_a Array }
+      end
+
+      describe 'total number of users' do
+        subject(:users) { auth0_api.get_users include_totals: true }
+
+        it { puts users }
+        it { is_expected.to be_a Hash }
+        it { is_expected.to have_key 'total' }
+        it { expect(users['total']).to be_a Integer }
+        it { expect(users['total']).to be >= 0 }
+        it { puts "Total number of users: #{users['total']}" }
+      end
+    end
+
+    describe '#create_user' do
+      context 'user does not exist' do
+        before(:all) do
+          @name = 'User Name'
+          @email = 'example@email.com'
+          @password = 'Password123'
+          @options = {
+            connection: 'Username-Password-Authentication',
+            email: @email,
+            password: @password,
+            name: @name,
+            email_verified: false,
+            verify_email: false,    # Prevent confirmation email
+            user_metadata: {},
+            app_metadata: {}
+          }
+          expect { @user = @auth0.create_user(@name, @options) }.not_to raise_exception
+        end
+
+        after(:all) do
+          return unless @user
+          user_id = @user['user_id']
+          @auth0.delete_user(user_id)
+        end
+
+        let(:name)  { @name }
+        let(:email) { @email }
+
+        subject(:user) { @user }
+
+        it { is_expected.not_to be_nil }
+        it { is_expected.to be_a Hash }
+        it { puts JSON.pretty_generate user.to_json }
+        it { expect(user).to have_key('user_id') }
+        it { expect(user).to have_key('name') }
+        it { expect(user).to have_key('email') }
+        it { expect(user).to have_key('email_verified') }
+        it { expect(user).to have_key('user_metadata') }
+        it { puts user['user_id'] }
+        it { expect(user['name']).to eq name }
+        it { expect(user['email']).to eq email }
+      end
+    end
+  end
+end
